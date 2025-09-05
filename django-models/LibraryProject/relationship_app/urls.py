cb7c1827fbb8e6bdf032e79f1a18a91407bb461d
MEKLIT MULUGETA TAFFA <meklitmulugeta71@gmail.com>
2025-09-05 17:58:35 +0300

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path('librarian-view/', views.librarian_view, name='librarian_view'),
     path('member-view/', views.member_view, name='member_view'),
     path('add_book/', views.add_book, name='add_book'),
-    path('change_book/<int:pk>/', views.edit_book, name='change_book'),
+    path('edit_book/<int:pk>/', views.edit_book, name='edit_book'),
     path('delete_book/<int:pk>/', views.delete_book, name='delete_book'),
 ]
 
