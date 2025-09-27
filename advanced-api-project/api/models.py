017c4ec08186c7127131420aae05d57c13ddb0dd
MEKLIT MULUGETA TAFFA <meklitmulugeta71@gmail.com>
2025-09-27 10:12:32 +0300

    Update models.py

--- advanced-api-project/api/models.py
+++ advanced-api-project/api/models.py
@@ -1,18 +1,18 @@
 from django.db import models
 
-# Create your models here.
 # Author model represents book authors
-class  Author(models.Model):
-  name = models.CharField(max_length=100)
-  def __str__(self):
-    return self.name
-  
-# Book model stores books and links to Author
-  
-class Book(models.Model):
-  title = models.TextField()
-  publication_year = models.IntegerField()
-  author = models.ForeignKey(Author,on_delete=models.CASCADE, related_name= "books")
+class Author(models.Model):
+    name = models.CharField(max_length=100)
 
-  def __str__(self):
-    return self.title
+    def __str__(self):
+        return self.name
+
+
+# Book model stores books and links to Author
+class Book(models.Model):
+    title = models.CharField(max_length=255)   
+    publication_year = models.IntegerField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="books")
+
+    def __str__(self):
+        return self.title
